8e61965dd6ab1ee2732081350837d8eb3cd9af22
3030744 <3030744@30307-44>
2023-01-06 14:56:30 +0900

    aaaa

--- 商品管理システム/baiten/aaaa.java
+++ 商品管理システム/baiten/aaaa.java
@@ -1,14 +1,22 @@
 package baiten;
 
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class aaaa extends JPanel {
+	private JTextField textField;
 
 	/**
 	 * Create the panel.
 	 */
 	public aaaa() {
+		
+		textField = new JTextField();
+		add(textField);
+		textField.setColumns(10);
 
 	}
+	
+	
 
 }
